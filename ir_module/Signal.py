41f6fe6943d7f6de77f0de6cb9369dec18f9511a
Robin Su <robisu@pdx.edu>
2022-03-16 16:54:11 -0700

    refactor funcs

--- ir_module/Signal.py
+++ ir_module/Signal.py
@@ -1,18 +1,14 @@
 from distutils.log import error
 import scipy.signal as signal
 import math
-from .default import *
-from .graph import graph_stft
-from .impulse_response import deconvolve_invfilt
-import avg_signal as avg
-import convolve as conv
+from . import avg_signal as avg, convolve, default, gen_sine as gs, graph, impulse_response as ir
 
-# TODO: if resample is a downsample, should I include a a decimate(lp_filter func in convolve.py)?
-# TODO: how to properly extract IR from the deconvolved result? where do I truncate the signal before the actual impulse?
-# TODO: scale graphing function to have tighter bounds on the fft graph (how to scale Frequency (y axis?))
 
 class Signal:
-    def __init__(self, signal, sps=sample_rate, channels=channels, width=sample_width):
+    def __init__(self, signal, sps=default.sample_rate, channels=default.channels, width=default.sample_width):
+        '''
+        Initialize an instance of the Signal class. Includes parameters pertaining to the sampling rate and format of the data.
+        '''
         self._sample_rate = sps
         self._channels = channels
         self._sample_width = width
@@ -20,24 +16,43 @@ class Signal:
 
 
     def get_state(self):
+        '''
+        Returns the sample rate, channel count, and sample size (in bytes).
+        '''
         return self._sample_rate, self._channels, self._sample_width
-    
+
 
     def get_sps(self):
+        '''
+        Returns the sampling rate (in Hz) of the signal.
+        '''
         return self._sample_rate
     
+
     def get_signal(self):
+        '''
+        Returns the signal as a numpy array of floating-point values.
+        '''
         return self._signal
     
+
     def set_state(self, signal, sps, channels, width):
+        '''
+        Set or change the base parameters of the Signal instance.
+        '''
         self._sample_rate = sps
         self._channels = channels
         self._width = width
         self._signal = signal
 
+
     def set_signal(self, samples):
+        '''
+        Set or change the signal within the Signal instance.
+        '''
         self._signal = samples
     
+
     def copy(self):
         cp_rate, cp_chan, cp_width = self.get_state()
         cp_signal = self._signal
@@ -46,6 +61,7 @@ class Signal:
 
         return copied
     
+
     def resample(self, new_sps):
         '''
         Up- or down-sample signal to new_sps sampling rate
@@ -75,11 +91,12 @@ class Signal:
         Assumes the Signal instance is a dry audio signal, convolve with given room impulse response to get a transformed signal.
         If Signal and impulse_response are not of same sampling rate, the impulse_response will be resampled to match the Signal
         :impulse_response: a Signal instance, containing a numpy array of floating-point values representing a room impulse response
-        Returns a numpy array of floating-point values representing the transformed signal
+        Returns a Signal instance containing the transformed signal
         '''
         assert self._sample_rate == impulse_response.get_sps(), f'impulse_response and signal sampling rates must match: Signal: {self.get_sps()}; Impulse Response: {impulse_response.get_sps()}'
-        convolved = conv.convolve_audio(self._signal, impulse_response.get_signal())
-        return convolved
+        convolved = convolve.convolve_audio(self._signal, impulse_response.get_signal())
+
+        return Signal(convolved, self._sample_rate, self._channels, self._sample_width)
 
 
     def deconvolve(self, input_signal):
@@ -87,34 +104,51 @@ class Signal:
         Extracts the impulse response from this Signal instance, which is assumed to be a recording of input_signal.
         Signal and input_signal must have matching sampling rates
         :input_signal: a Signal instance, with original input signal from which this Signal instance was generated.
-        Returns a numpy array of floating-point values representing the extracted impulse response
+        Returns a Signal instance representing the extracted impulse response
         '''
         assert self._sample_rate == input_signal.get_sps(), f'input signal sampling rate must match Signal instance'
-        deconv = deconvolve_invfilt(self._signal, input_signal.get_signal(), mode='freq')
-        return deconv
+        deconv = ir.deconvolve_invfilt(self._signal, input_signal.get_signal(), mode='freq')
+
+        return Signal(deconv, self._sample_rate, self._channels, self._sample_width)
 
 
-def average_signal(directory, file_base=''):
-    pass
+def average_signal(directory, num_frames):
+    '''
+    See documentation in avg_signal.py for more information.
+    :directory: (string)
+    :num_frames: (int)
+    Returns a numpy array of floating-point values.
+    '''
+    return avg.xcorr_and_avg(directory, num_frames)
 
 
-def generate_sine(start=440, end=440, start_amp=0.75, end_amp=0.75, duration=1, mode='lin'):
-    pass
+def generate_sine(freq=440, amp=.75, duration=1, start=440, end=440, start_amp=0.75, end_amp=0.75, sr=default.sample_rate, mode='lin'):
+    '''
+    Generate a sine wave or sweep of given duration.
+    :freq: (int) frequency of standing sine wave
+    :amp: (int) amplitude of a standing sine wave
+    :duration: (int) duration of signal in seconds.
+    :start: (int) if generating a sine sweep, the starting frequency
+    :end: (int) if generating a sine sweep, the ending frequency
+    :start_amp: (float) if generating a sine sweep, the starting amplitude. Between 0 and 1.
+    :end_amp: (float) if generating a sine sweep, the ending amplitude. Between 0 and 1.
+    :sr: (int) sampling rate (in Hz)
+    :mode: ['sine', 'lin', 'exp'] Function mode. 'sine' will generate a sine wave of constant frequency. 'lin' and 'exp' will generate a sine sweep, of linear or exponential frequency growth, respectively. 
+    Returns a numpy array of floating-point samples.
+    '''
+    samples = None
+    if mode == 'sine':
+        samples = gs.sine_wave(freq, duration, sample_rate=sr, amp=amp)
+    elif mode == 'lin':
+        samples = gs.lin_sweep(start, end, duration, sample_rate=sr, st_amp=start_amp, end_amp=end_amp)
+    elif mode == 'exp':
+        samples = gs.exp_sweep(start, end, duration, sample_rate=sr, st_amp=start_amp, end_amp=end_amp)
+    
+    return samples
 
 
 def plot_fft(signal, save=False, fname=''):
     '''
     A utility function to plot the fft of the Signal instance. Option to save graph to given file.
     '''
-    graph_stft(signal, save=save, fname=fname)
-
-
-    '''
-    potential member functions:
-    - convolve(given impulse response file)
-    - generate sine(all the parameters)
-    - deconvolve (return can be impulse response)
-    - plot signal (either time or frequency domain)
-    non member functions:
-    - average signal from batch of files 
-    '''
+    graph.graph_stft(signal, save=save, fname=fname)
