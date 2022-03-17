71190baa70fce430d875fd2ad07c0bfc7a560a6c
Robin Su <robisu@pdx.edu>
2022-03-16 19:17:34 -0700

    file edit

--- demo_reverb.py
+++ demo_reverb.py
@@ -4,15 +4,15 @@ import numpy as np
 import pyaudio
 
 seconds = 5
-params, ir = wav_util.read_wave_nbit('demo/impulse_responses/garage_rir_nospkr_nocover.wav')
-start = np.argmax(ir)
+params, ir = wav_util.read_wave_nbit('demo/impulse_responses/garage_rir.wav')
+params, dry_audio = wav_util.read_wave_nbit('demo/dry_audio/bach_prelude_mono.wav')
 
+start = np.argmax(ir)
 impulse = ir[start:start+(default.sample_rate * seconds)]
 impulse = np.append(np.zeros(1000), impulse)
-# impulse = impulse * .5 # scale impulse to avoid clipping (which can cause pops in audio playback)
 ibytes = wav_util.samples_to_bytes_16bit(impulse)
 
-params, dry_audio = wav_util.read_wave_nbit('demo/dry_audio/bach_prelude_mono.wav')
+
 audio = Signal(dry_audio, params.framerate, params.nchannels, params.sampwidth)
 rir = Signal(impulse, params.framerate, params.nchannels, params.sampwidth)
 
